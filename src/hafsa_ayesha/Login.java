9a4f4da8104948a4ba94e508ef62434e6ec0bace
Ayesha Naseer <ayeshanaseer19999@gmail.com>
2024-11-14 10:47:47 +0500

    updated first time

--- src/hafsa_ayesha/Login.java
+++ src/hafsa_ayesha/Login.java
@@ -1,7 +1,9 @@
 package hafsa_ayesha;
 
 public class Login {
-    public boolean authenticate(String username, String password) {
-        return username.equals("admin") && password.equals("password123");
-    }
+	public boolean authenticate(String username, String password) {
+	    System.out.println("Authenticating user...");
+	    return username.equals("admin") && password.equals("password123");
+	}
+
 }
