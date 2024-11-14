200b1448000c8a37f4badbd84ff861f4178f937d
Ayesha Naseer <ayeshanaseer19999@gmail.com>
2024-11-14 11:15:11 +0500

    update main class

--- src/hafsa_ayesha/Main.java
+++ src/hafsa_ayesha/Main.java
@@ -1,16 +1,15 @@
 package hafsa_ayesha;
-public class Main {
-    public static void main(String[] args) {
-<<<<<<< HEAD
-       
-        System.out.println("Main branch update.");
-       
-=======
-    	 System.out.println("Feature A work in progress.");
->>>>>>> e5ce516 (update main class)
-    }
 
-    public static void greetUser() {
-        System.out.println("Hello, User!");
-    }
+public class Main {
+	public static void main(String[] args) {
+
+		System.out.println("Main branch update.");
+
+		System.out.println("Feature A work in progress.");
+
+	}
+
+	public static void greetUser() {
+		System.out.println("Hello, User!");
+	}
 }
