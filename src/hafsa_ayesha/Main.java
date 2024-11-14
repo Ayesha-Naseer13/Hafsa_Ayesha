aab0ec165cd7a83520a5a87fc58577bfa5d3967e
Ayesha Naseer <ayeshanaseer19999@gmail.com>
2024-11-14 11:07:25 +0500

    Fix critical bug in Main class

--- src/hafsa_ayesha/Main.java
+++ src/hafsa_ayesha/Main.java
@@ -2,6 +2,7 @@ package hafsa_ayesha;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to the application!");
+        System.out.println("fixed the bug!");
         greetUser();
     }
 
